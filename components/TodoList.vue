a8c3f1aa6c62efc8c3d27220d9a092e1efc39e5f
Dmitriy <rsp.fdrf@yandex.ru>
2022-07-27 21:35:06 +0500

    Created the first components

---
+++ components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+<ul>
+    <Todoitem />
+</ul>
+
+</div>
+
+</template>
+
+<script>
+import TodoitemVue from './Todoitem.vue'
+import Todoitem from './Todoitem.vue'
+export default {
+    components: {
+    TodoitemVue,
+    Todoitem
+} 
+}
+</script>
